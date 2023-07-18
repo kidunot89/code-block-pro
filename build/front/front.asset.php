ca9072b54b176c8fc366cddd414977d585d94de8
Geoff Taylor <geoff@axistaylor.com>
2023-07-17 20:40:13 -0400

    devops: build versioned for temporary distribution

---
+++ build/front/front.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b3a59fe041d4fa563035');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '783680be6b829e18887a');
